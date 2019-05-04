339cc382516ccd42609643a3c650d3d59fd33122
michal bojanowicz <boramaapps@gmail.com>
2019-05-04 23:35:34 +0200

    Initial Commit

---
+++ AUHost/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AUHost
+//
+//  Created by michal on 04/05/2019.
+//  Copyright © 2019 borama. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
