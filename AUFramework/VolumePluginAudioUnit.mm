f5fb5a6e521acff67874d9befb6cad8a13b12ddc
michal bojanowicz <standinga@o2.pl>
2019-09-23 03:59:15 +0200

    bug fix: dataSize was set to byteSize of buffer, not the actual number of samples of buffer.

--- AUFramework/VolumePluginAudioUnit.mm
+++ AUFramework/VolumePluginAudioUnit.mm
@@ -151,10 +151,13 @@ const AudioUnitParameterID myParam1 = 0;
         
         float* input = (float*)inAudioBufferList->mBuffers[0].mData;
         float* output = (float*)outAudioBufferList->mBuffers[0].mData;
-        UInt32 dataSize = inAudioBufferList->mBuffers[0].mDataByteSize;
+        
+        UInt32 dataSize = inAudioBufferList->mBuffers[0].mDataByteSize / sizeof(float);
+        
+        // Samples are not interleaved. In stereo first package of samples of dataSize is left channgel, the next is the right channel
         for (int j = 0; j < inAudioBufferList->mNumberBuffers; j++) {
             for (int i = 0; i < dataSize; i++) {
-                output[i] = buffer->volume * input[i];
+                output[i + j * dataSize] = buffer->volume * input[i + j * dataSize];
             }
         }
         
